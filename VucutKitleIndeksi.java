50a6f14889e254adc9a475d4f256c1c49fe6e1f5
Hakan Karagöz <hknkrgzhkn@gmail.com>
2022-08-03 11:45:56 +0300

    VucutKitleIndeksi Eklendi

---
+++ VucutKitleIndeksi.java
@@ -0,0 +1,18 @@
+package Giris;
+import java.util.Scanner;
+public class VucutKitleIndeksi {
+    public static void main(String[] args) {
+        Scanner girdi= new Scanner(System.in);
+
+        double kilo, boy, indeks;
+
+        System.out.print("Boyunuz: ");
+        boy= girdi.nextDouble();
+
+        System.out.print("Kilonuz: ");
+        kilo=girdi.nextDouble();
+
+        indeks= kilo / (boy * boy);
+        System.out.println("Vücut Kitle İndeksiniz: " + indeks);
+    }
+}
