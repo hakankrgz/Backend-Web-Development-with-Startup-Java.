6c4d4093958a5ee8b59417fb3641befe4ba5f5c3
Hakan Karagöz <hknkrgzhkn@gmail.com>
2022-08-08 15:01:20 +0300

    başlangıç update

--- Baslangic.java
+++ Baslangic.java
@@ -112,5 +112,11 @@ public class Baslangic {
                     System.out.println("düşük derecede tuzlu");
                 }
         }
+
+        int i=1;
+        while (i<=5){
+            System.out.println(i);
+            ++i;
+        }
     }
 }
