2d53cffc03b865e37763c4f20dd245dbd89296d5
Hakan Karagöz <hknkrgzhkn@gmail.com>
2022-08-10 17:35:15 +0300

    Bir Sayinin Basamak Sayilarini Toplamini Bulan Program

---
+++ BirSayininBasamakSayilariniToplaminiBulanProgram.java
@@ -0,0 +1,20 @@
+package Giris;
+
+import java.util.Scanner;
+
+public class BirSayininBasamakSayilariniToplaminiBulanProgram {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Bir sayı girin: ");
+        int number = inp.nextInt();
+        int endNum;
+        int resualt = 0;
+        int tempnumber = number;
+        while (tempnumber != 0) {
+            endNum = tempnumber % 10;
+            tempnumber /= 10;
+            resualt += endNum;
+        }
+        System.out.println("Basamakları toplamı: " + resualt);
+    }
+}
