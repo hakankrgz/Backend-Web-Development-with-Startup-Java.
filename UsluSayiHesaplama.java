8172533fddfdce41ea485a7e4bc568803df96885
Hakan Karagöz <hknkrgzhkn@gmail.com>
2022-08-10 11:12:38 +0300

    Üslü Sayı Hesaplama Programı Eklendi

---
+++ UsluSayiHesaplama.java
@@ -0,0 +1,14 @@
+package Giris;
+
+import java.util.Scanner;
+
+public class UsluSayiHesaplama {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Enter the üssü alınacak number: ");
+        int n = inp.nextInt();
+        System.out.print("Enter the üs number: ");
+        int k = inp.nextInt();
+        int total = 1;
+    }
+}
