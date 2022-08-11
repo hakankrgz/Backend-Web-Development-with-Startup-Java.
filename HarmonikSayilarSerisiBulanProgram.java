1bf7c28f0c28a42909c274dcc4d5a8e15ac612b3
Hakan Karagöz <hknkrgzhkn@gmail.com>
2022-08-11 08:56:47 +0300

    HarmonikSayilarSerisiBulanProgram Eklendi

---
+++ HarmonikSayilarSerisiBulanProgram.java
@@ -0,0 +1,17 @@
+package Giris;
+
+import java.util.Scanner;
+
+public class HarmonikSayilarSerisiBulanProgram {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        double n;
+        double result=0;
+        System.out.print("N sayısını girin: ");
+        n = inp.nextInt();
+        for (double i=1;i<=n;i++){
+            result+=1/i;
+        }
+        System.out.println(result);
+    }
+}
